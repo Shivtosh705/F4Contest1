8cc2ab5d0bc67c5ea3e5df9a4fa447549ee911ba
Shivtosh706 <shivtoshmishra005@gmail.com>
2023-06-17 23:58:25 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "./Component/Form";
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Form/>
+    </div>
+  );
+};
+
+
+export default App;
